ddba197a5df389ec8c60c1c4307e5c1ff1ab7ee9
Ana Costa Eduardo <ana.costa.eduardo@gmail.com>
2020-04-20 12:03:22 -0300

    Remoção de arquivos desnecessários + criação da página Home e de um componente-modelo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <h1 className="Logo">Fashionista</h1>
+  );
+}
+
+export default Logo;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+import Logo from '../../components/Logo/Logo';
+
+const Home = () => {
+  return (
+    <div className='Home'>
+      <Logo />
+    </div>
+  );
+}
+
+export default Home;
