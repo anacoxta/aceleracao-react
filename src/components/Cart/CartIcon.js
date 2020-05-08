e052ff830f98ebc6fcef4052b204359c6767bc1d
Isabella <isabella.soares.28@gmail.com>
2020-05-07 22:12:34 -0300

    Create Cart Icon component

---
+++ src/components/Cart/CartIcon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CartIcon = () => {
+  return (
+    <>
+      <span>
+        <i class="fas fa-shopping-cart"></i>
+      </span>
+    </>
+  );
+}
+
+export default CartIcon;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Header.css';
+import CartIcon from '../Cart/CartIcon';
 
 const Header = () => {
   return (
-    <header className='header'></header>
+    <header className='header'>
+      <CartIcon />
+    </header>
   );
 }
 
