87f86b8fa0c2483c47ad0d2b91a70fd4ca20b6bf
Isabella <isabella.soares.28@gmail.com>
2020-05-07 22:26:20 -0300

    Add logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css';
+import Logo from '../Logo/Logo'
 import CartIcon from '../Cart/CartIcon';
 
 const Header = () => {
   return (
     <header className='header'>
+      <Logo />
       <CartIcon />
     </header>
   );
