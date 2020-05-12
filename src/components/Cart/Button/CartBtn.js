3f0cd6c285fac18a78904450de0b997fc252bcb7
Isabella <isabella.soares.28@gmail.com>
2020-05-12 17:54:58 -0300

    Increasing accessibility

--- src/components/Cart/Button/CartBtn.js
+++ src/components/Cart/Button/CartBtn.js
@@ -3,7 +3,11 @@ import './CartBtn.scss';
 
 const CartBtn = ({ activeCart, setActiveCart }) => {
   return (
-    <button className='cart__button' data-count='5' onClick={() => setActiveCart(!activeCart)}>
+    <button
+      className='cart__button'
+      data-count='5'
+      aria-label='Abrir carrinho de compras'
+      onClick={() => setActiveCart(!activeCart)}>
       <i className='fas fa-shopping-cart'></i>
     </button>
   );
--- src/components/Cart/Container/CartContainer.js
+++ src/components/Cart/Container/CartContainer.js
@@ -4,8 +4,14 @@ import './CartContainer.scss';
 const CartContainer = ({ activeCart, setActiveCart }) => {
   return (
     <div className={activeCart ? 'cart__container cart__container--display' : 'cart__container'}>
-      <div className={activeCart ? 'cart__container--active' : ''}>
-        <button className='cart__container__button' onClick={() => setActiveCart(!activeCart)}>
+      <div
+        className={activeCart ? 'cart__container--active' : ''}
+        role='dialog'
+        aria-labelledby='Carrinho de compras'>
+        <button
+          className='cart__container__button'
+          aria-label='Fechar carrinho de compras'
+          onClick={() => setActiveCart(!activeCart)}>
           <i class="fas fa-times-circle"></i>
         </button>
       </div>
