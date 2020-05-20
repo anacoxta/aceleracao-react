e4bfbc38509cf7a8d0cf43d5fb679c9f7852636b
Bruna G Paula <brunagabriele.paula@gmail.com>
2020-05-20 12:26:44 -0300

    Corrects semantic structure of cards for list and inserts new attributes to reference the product in an accessible way

--- src/components/CardProduct/CardProduct.js
+++ src/components/CardProduct/CardProduct.js
@@ -4,21 +4,24 @@ import Image from '../../assets/images/product.png'
 
 const CardProduct = () => {
     return (
-      <figure className="card-product-default">
-        <a href="#" className="card-product-default__info">
-          <div className="card-product-default__inner">
-            <img src={Image} alt={'alt'}/>
-            <div className="card-product-default__inner card-product-default__inner--price-discount no-discount">
-              <h3>30% OFF</h3>
+      <ul className="card-product-list">
+        <li className="card-product-default" aria-labelledby="product-description">
+          <span id="product-description"></span>
+          <a href="#" className="card-product-default__info" aria-label="Detalhe do produto">
+            <div className="card-product-default__inner">
+              <img src={Image} alt={'alt'}/>
+              <div className="card-product-default__inner card-product-default__inner--price-discount no-discount">
+                <h3>30% OFF</h3>
+              </div>
+              <div className="card-product-default__inner card-product-default__inner--pricing">
+              {/* [componente title description] */}
+                <h2>NOME DO PRODUTO</h2>
+                <p>R$ 69,90 <span className="no-remarcable">R$ 99,00</span></p>
+              </div>
             </div>
-            <ficaption className="card-product-default__inner card-product-default__inner--pricing">
-            {/* [componente title description] */}
-              <h2>NOME DO PRODUTO</h2>
-              <p>R$ 69,90 <span className="no-remarcable">R$ 99,00</span></p>
-            </ficaption>
-          </div>
-        </a>
-      </figure>
+          </a>
+        </li>
+      </ul>
     )
 }
 
