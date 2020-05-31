612c42870c010b237e7fc754c60931c5e623ff0c
vanessa.ariel <vanessa.ariel@wunderman.com>
2020-05-31 20:27:03 -0300

    [RWLT-36] - Move media query position, add single quotes

--- src/components/ProductDescription/ProductDescription.js
+++ src/components/ProductDescription/ProductDescription.js
@@ -1,21 +1,21 @@
-import React from "react";
-import "./ProductDescription.scss";
+import React from 'react';
+import './ProductDescription.scss';
 
 const ProductDescription = () => {
   return (
-      <section className="productDescription isInsideCart">
-        <p className="productDescription__name">NOME DO PRODUTO</p>
-        <div className="productDescription__pricingGroup">
-          <div className="productDescription__actual">
-            <span className="productDescription__price--actual">R$ 69,90</span>
-            <span className="productDescription__installments">
-              {" "}
+      <section className='productDescription isInsideCart'>
+        <p className='productDescription__name'>NOME DO PRODUTO</p>
+        <div className='productDescription__pricingGroup'>
+          <div className='productDescription__actual'>
+            <span className='productDescription__price--actual'>R$ 69,90</span>
+            <span className='productDescription__installments'>
+              {' '}
               3x R$23,30
             </span>
           </div>
-          <div className="productDescription__reg">
-          <span className="productDescription__price--reg">R$ 109,90</span>
-          <span className="productDescription__discountPercent">(desconto de 30%)</span>
+          <div className='productDescription__reg'>
+          <span className='productDescription__price--reg'>R$ 109,90</span>
+          <span className='productDescription__discountPercent'>(desconto de 30%)</span>
           </div>
         </div>
       </section>
