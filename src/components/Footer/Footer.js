79456af1be8a0349597c623e245d37e38587008d
anacoxta <ana.costa.eduardo@gmail.com>
2020-06-06 16:19:48 -0300

    [RWLT-58] Create footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__leftBlock">
+        <span className="footer__leftBlock__text footer__leftBlock__text--course">
+          Aceleração React Women
+        </span>
+        <span className="footer__leftBlock__text footer__leftBlock__text--divider">
+          &nbsp;|&nbsp;
+        </span>
+        <span className="footer__leftBlock__text footer__leftBlock--sponsors">
+          <a className="link" href="https://codenation.dev/">Codenation</a>
+          &nbsp;+&nbsp;
+          <a className="link" href="https://loft.com.br/">Loft</a>
+        </span>
+      </div>
+      <a className="footer__rightBlock link" href="https://github.com/anacoxta/aceleracao-react">
+        <i class="footer__rightBlock__icon fab fa-github"></i>
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
