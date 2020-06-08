38c3bff528874ce2a1d0eca711b75cb1b98d3614
Lia Freitas Lobo <lia.lobo@gmail.com>
2020-06-08 09:25:07 -0300

    [RWLT-60] add request in home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.scss';
 import Logo from '../../components/Logo/Logo';
+import { getCatalog } from '../../services/catalog'
 
 const Home = () => {
+  const [catalog, setCatalog] = useState([])
+
+  useEffect(() => {
+    getCatalog()
+      .then(resp => setCatalog(resp.data));
+  }, []);
+  // console.log(catalog)
+
   return (
     <div className='Home'>
       <Logo />
