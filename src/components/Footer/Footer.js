c28425fec14d90fc5585e376b99ce289f4465ece
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-11 16:25:46 -0300

    fix: className

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,22 +3,26 @@ import './Footer.scss';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer__leftBlock">
-        <span className="footer__leftBlock__text footer__leftBlock__text--course">
+    <footer className='footer'>
+      <div className='footer__leftBlock'>
+        <span className='footer__leftBlock__text footer__leftBlock__text--course'>
           Aceleração React Women
         </span>
-        <span className="footer__leftBlock__text footer__leftBlock__text--divider">
+        <span className='footer__leftBlock__text footer__leftBlock__text--divider'>
           &nbsp;|&nbsp;
         </span>
-        <span className="footer__leftBlock__text footer__leftBlock--sponsors">
-          <a className="link" href="https://codenation.dev/">Codenation</a>
+        <span className='footer__leftBlock__text footer__leftBlock--sponsors'>
+          <a className='link' href='https://codenation.dev/'>
+            Codenation
+          </a>
           &nbsp;+&nbsp;
-          <a className="link" href="https://loft.com.br/">Loft</a>
+          <a className='link' href='https://loft.com.br/'>
+            Loft
+          </a>
         </span>
       </div>
-      <a className="footer__rightBlock link" href="https://github.com/anacoxta/aceleracao-react">
-        <i class="footer__rightBlock__icon fab fa-github"></i>
+      <a className='footer__rightBlock link' href='https://github.com/anacoxta/aceleracao-react'>
+        <i className='footer__rightBlock__icon fab fa-github'></i>
       </a>
     </footer>
   );
