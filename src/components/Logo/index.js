ecf1aa7ef815b228c9e0ca76c1ab9a8ebdd9fd1f
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-13 15:07:20 -0300

    refactor: logo component

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './style.scss';
 
-const Logo = () => {
-  return <span className='Logo'>Fashionista</span>;
-};
+const Logo = () => <span className='Logo'>Fashionista</span>;
 
 export default Logo;
