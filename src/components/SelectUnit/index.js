80ae8ac395419df9747011b3a10c98aac20e17b7
lia.lobo-ext <lia.lobo-ext@anbima.com.br>
2020-06-16 23:27:11 -0300

    [RWLT-56]create function for onclick plus and minus

--- src/components/SelectUnit/index.js
+++ src/components/SelectUnit/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './style.scss';
 
 const SelectUnit = () => {
+  const [unitNumber, setUnitNumber] = useState(0)
+
+  const handleMinusClick = () => {
+    return unitNumber !== 0 ? setUnitNumber(unitNumber - 1) : unitNumber
+  }
+
+  const handlePlusClick = () => {
+    setUnitNumber(unitNumber + 1)
+  }
+
   return (
     <div className='containerUnit'>
-      <button className='buttonUnit buttonUnit__fontSize--minus'>
+      <button className='buttonUnit buttonUnit__fontSize--minus' onClick={handleMinusClick}>
         -
       </button>
-      <span> 2 </span>
-      <button className='buttonUnit buttonUnit__fontSize--plus'>
+      <span> {unitNumber} </span>
+      <button className='buttonUnit buttonUnit__fontSize--plus' onClick={handlePlusClick}>
         +
       </button>
     </div>
