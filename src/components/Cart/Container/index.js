ea4e87027f59a68174fb8325de45a6c6a95d3b6d
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-18 14:05:47 -0300

    feat: add cart buttons

--- src/components/Cart/Container/index.js
+++ src/components/Cart/Container/index.js
@@ -1,27 +1,40 @@
-import React from "react";
-import "./style.scss";
+import React from 'react';
+import './style.scss';
+
+import ButtonCta from '../../ButtonCta';
 
 const CartContainer = ({ activeCart, setActiveCart }) => {
+  const handleKeepBuyingBtn = () => {
+    alert('Continue comprando!');
+  };
+
+  const handlePaymentBtn = () => {
+    alert('Redirecionando para o pagamento!');
+  };
+
   return (
-    <div
-      className={
-        activeCart
-          ? "cart__container cart__container--display"
-          : "cart__container"
-      }
-    >
+    <div className={activeCart ? 'cart__container cart__container--display' : 'cart__container'}>
       <div
-        className={activeCart ? "cart__container--active" : ""}
-        role="dialog"
-        aria-label="Carrinho de compras"
+        className={activeCart ? 'cart__container--active' : ''}
+        role='dialog'
+        aria-label='Carrinho de compras'
       >
         <button
-          className="cart__container__button"
-          aria-label="Fechar carrinho de compras"
+          className='cart__container__button'
+          aria-label='Fechar carrinho de compras'
           onClick={() => setActiveCart(!activeCart)}
         >
-          <i className="fas fa-times-circle"></i>
+          <i className='fas fa-times-circle'></i>
         </button>
+
+        <div className='cart__container__btnGroup'>
+          <ButtonCta
+            text='Continuar comprando'
+            layout='default'
+            handleClick={handleKeepBuyingBtn}
+          />
+          <ButtonCta text='Ir para o pagamento' layout='filled' handleClick={handlePaymentBtn} />
+        </div>
       </div>
     </div>
   );
