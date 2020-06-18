1f736d3621c99380978a834b9c3abaeb5787acd8
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-18 13:39:52 -0300

    feat: create button component

---
+++ src/components/ButtonCta/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.scss';
+
+const CardProduct = (text, handleClick, style) => {
+  return <button className={`buttonCta buttonCta--${style}`}>Button cta</button>;
+};
+
+export default CardProduct;
