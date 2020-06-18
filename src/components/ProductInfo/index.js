0fe82d2f9ec9442aa0ef19a5c920d9873fa2dba4
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-18 14:25:56 -0300

    feat: add bag button

--- src/components/ProductInfo/index.js
+++ src/components/ProductInfo/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './style.scss';
 
+import ButtonCta from '../ButtonCta';
 import ProductDescription from '../ProductDescription';
 import SizeSelect from '../SizeSelect';
 
 const ProductInfo = () => {
+  const handleBagBtn = () => {
+    alert('Adicionando à sacola!');
+  };
+
   return (
-    <div>
+    <div className='productInfo'>
       <ProductDescription />
       <SizeSelect />
+      <ButtonCta
+        text='Adicionar à sacola de compras'
+        layout='filled productInfo__buttonCta'
+        handleClick={handleBagBtn}
+      />
     </div>
   );
 };
