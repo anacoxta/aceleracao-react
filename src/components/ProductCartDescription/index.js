03ccd0eb951a6cafbee43375b086d23a0b062257
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-21 12:26:28 -0300

    refactor: create product cart description component

---
+++ src/components/ProductCartDescription/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.scss';
+
+const ProductCartDescription = () => {
+  return (
+    <section className='productCartDescription isInsideCart'>
+      <p className='productCartDescription__name'>NOME DO PRODUTO</p>
+      <div className='productCartDescription__pricingGroup'>
+        <div className='productCartDescription__actual'>
+          <span className='productCartDescription__price--actual'>R$ 69,90</span>
+          <span className='productCartDescription__installments'>em até 3x R$23,30</span>
+        </div>
+        <div className='productCartDescription__reg'>
+          <span className='productCartDescription__price--reg'>R$ 109,90</span>
+          <span className='productCartDescription__discountPercent'>(desconto de 30%)</span>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProductCartDescription;
