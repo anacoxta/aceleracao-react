40f1ed6fa0b8e6c3b7ea4604985a88405576e019
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-23 15:38:02 -0300

    WIP reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,32 +1,25 @@
 import { ADD_PRODUCT, REMOVE_PRODUCT } from '../actions';
 
 const initialState = {
-  precoTotal: 0,
-  quantTotal: 0,
+  totalPrice: 0,
+  totalAmount: 0,
   products: [],
 };
-
-// {
-//   id,
-//   quantidade,
-//   preco,
-//   tamanho
-// }
 
 export const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_PRODUCT:
       return {
         ...state,
-        precoTotal: state.precoTotal + action.newProduct.amount * action.newProduct.price,
-        quantTotal: state.quantTotal + action.newProduct.amount,
+        totalPrice: state.totalPrice + action.newProduct.amount * action.newProduct.price,
+        totalAmount: state.totalAmount + action.newProduct.amount,
         products: [...state.products, action.newProduct],
       };
     case REMOVE_PRODUCT:
       return {
         ...state,
-        precoTotal: state.precoTotal - action.deletedProduct.amount * action.deletedProduct.price,
-        quantTotal: state.quantTotal - action.deletedProduct.amount,
+        totalPrice: state.totalPrice - action.deletedProduct.amount * action.deletedProduct.price,
+        totalAmount: state.totalAmount - action.deletedProduct.amount,
         products: state.products.map((product) => {
           if (product.id === action.deletedProduct.id) {
             return {
