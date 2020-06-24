45b173268dc7644f3bdca46af60a6356925f96f5
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-24 16:54:51 -0300

    feat: add store to the cart button

--- src/components/Cart/Button/index.js
+++ src/components/Cart/Button/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './style.scss';
 
-const CartBtn = ({ activeCart, setActiveCart }) => {
+const CartBtn = (props) => {
+  const { cartAmount, setActiveCart, activeCart } = props;
+
   return (
     <button
       className='cart__button'
-      data-count='5'
+      data-count={cartAmount}
       aria-label='Abrir carrinho de compras'
-      onClick={() => setActiveCart(!activeCart)}>
+      onClick={() => setActiveCart(!activeCart)}
+    >
       <i className='fas fa-shopping-cart'></i>
     </button>
   );
-}
+};
 
-export default CartBtn;
+const mapStateToProps = (store) => ({
+  cartAmount: store.totalAmount,
+});
+
+export default connect(mapStateToProps)(CartBtn);
