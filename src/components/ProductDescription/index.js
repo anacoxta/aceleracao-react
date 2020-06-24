98c51141189d186ab983ea90730620f92c297c93
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-23 21:25:26 -0300

    fix: product description to details page

--- src/components/ProductDescription/index.js
+++ src/components/ProductDescription/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './style.scss';
 
-const ProductDescription = () => {
+const ProductDescription = ({ layout }) => {
   return (
-    <section className='productDescription isInsideCart'>
+    <section className={`productDescription ${layout}`}>
       <p className='productDescription__name'>NOME DO PRODUTO</p>
       <div className='productDescription__pricingGroup'>
         <div className='productDescription__actual'>
           <span className='productDescription__price--actual'>R$ 69,90</span>
           <span className='productDescription__installments'>3x R$23,30</span>
         </div>
-        <div className='productDescription__reg'>
-          {/* <span className='productDescription__price--reg'>R$ 109,90</span> */}
-          {/* <span className='productDescription__discountPercent'>(desconto de 30%)</span> */}
-        </div>
+
+        {layout === 'isInsideProduct' && (
+          <div className='productDescription__reg'>
+            <span className='productDescription__price--reg'>R$ 109,90</span>
+            <span className='productDescription__discountPercent'>(desconto de 30%)</span>
+          </div>
+        )}
       </div>
     </section>
   );
