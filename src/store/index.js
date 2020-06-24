374462e6b7aa4b96eb236703898fb206ddf28c6b
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-24 16:53:54 -0300

    fix: correct reducer name

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
 import { createStore } from 'redux';
-import { Reducers } from '../reducers';
+import { cartReducer } from '../reducers';
 
-export const Store = createStore(Reducers);
+export const Store = createStore(cartReducer);
