7e19df722fee3c7b7b8d551b9ea1f99767a43bad
vanessa.ariel <vanessa.ariel@wunderman.com>
2020-06-27 18:48:01 -0300

    [RWLT-81] Add vertical scroll to products list, move buttons from cart container to cart modal

--- src/components/Cart/Container/index.js
+++ src/components/Cart/Container/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './style.scss';
 import CartModal from '../Modal';
-import ButtonCta from '../../ButtonCta';
-import { handleKeepBuyingBtn, handlePaymentBtn } from '../../../utils/buttonFunctions';
 
 const CartContainer = ({ activeCart, setActiveCart, totalPrice }) => {
   return (
@@ -13,23 +11,7 @@ const CartContainer = ({ activeCart, setActiveCart, totalPrice }) => {
         role='dialog'
         aria-label='Carrinho de compras'
       >
-        <CartModal activeCart={activeCart} setActiveCart={setActiveCart} />
-        <div className='cart__container__group'>
-          <div className='cart__container__subtotal'>
-            <span>Subtotal</span>
-            <span className='cart__container__subtotalPrice'>
-              {totalPrice.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
-            </span>
-          </div>
-          <div className='cart__container__btnGroup'>
-            <ButtonCta
-              text='Continuar comprando'
-              layout='default'
-              handleClick={handleKeepBuyingBtn}
-            />
-            <ButtonCta text='Finalizar pagamento' layout='filled' handleClick={handlePaymentBtn} />
-          </div>
-        </div>
+        <CartModal activeCart={activeCart} setActiveCart={setActiveCart} totalPrice={totalPrice}/>
       </div>
     </div>
   );
