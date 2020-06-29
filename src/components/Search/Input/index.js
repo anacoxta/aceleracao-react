c78d65336c4379916b8b8634bf319b0503feded2
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-29 13:57:06 -0300

    fix: remove unused imports

--- src/components/Search/Input/index.js
+++ src/components/Search/Input/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.scss';
 
 import { getCatalog } from '../../../services/catalog';
