16a7978aaeab44059a44d087a70d224bce8c9b55
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-29 08:58:29 -0300

    refactor: add Header and Footer to routes component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as ServiceWorker from './serviceWorker';
 import './index.scss';
-import Header from './components/Header';
-import Footer from './components/Footer';
+
 import Routes from './routes';
 import { Provider } from 'react-redux';
 import { Store } from './store';
@@ -11,9 +10,7 @@ import { Store } from './store';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={Store}>
-      <Header />
       <Routes />
-      <Footer />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
