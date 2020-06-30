12ac02a7c33ceaa01a72d44ff59d65d59fcfed25
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-30 10:35:58 -0300

    fix: solve problems with parentheses

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,7 +5,7 @@ import Loading from '../../components/Loading';
 import { getCatalog } from '../../services/catalog';
 
 import '../defaultStyles.scss';
-import './index.scss';
+import './style.scss';
 
 const Home = () => {
   const [catalog, setCatalog] = useState(null);
@@ -16,26 +16,22 @@ const Home = () => {
 
   return (
     <div className='pageContent pageContentHome'>
-      {
-        catalog && catalog.map(item => {
-          return (
-            <CardProduct
-              key={item.code_color}
-              src={item.image}
-              alt={`${item.name}`}
-              discountPrice={
-                item.discount_percentage !== '' ? `${item.discount_percentage} OFF` : ''
-              }
-              className={item.on_sale ? 'discount' : 'discount--none'}
-              layout='isInsideHome'
-              name={item.name}
-              onSale={item.on_sale}
-              actualPrice={item.actual_price}
-              regularPrice={item.regular_price}
-              productId={item.code_color}
-            />
-          );
-        })
+      {catalog ? (
+        catalog.map((item) => (
+          <CardProduct
+            key={item.code_color}
+            src={item.image}
+            alt={`${item.name}`}
+            discountPrice={item.discount_percentage !== '' ? `${item.discount_percentage} OFF` : ''}
+            className={item.on_sale ? 'discount' : 'discount--none'}
+            layout='isInsideHome'
+            name={item.name}
+            onSale={item.on_sale}
+            actualPrice={item.actual_price}
+            regularPrice={item.regular_price}
+            productId={item.code_color}
+          />
+        ))
       ) : (
         <Loading />
       )}
