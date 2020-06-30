046ec66f22f125bed0767178edaa2ff5f54bea8d
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-30 10:38:29 -0300

    remove comments

--- src/components/ProductDescription/index.js
+++ src/components/ProductDescription/index.js
@@ -3,30 +3,20 @@ import classnames from 'classnames';
 
 import './style.scss';
 
-const ProductDescription = ({ layout, name, regularPrice, actualPrice, installments, onSale}) => {
+const ProductDescription = ({ layout, name, regularPrice, actualPrice, installments, onSale }) => {
   return (
     <section className={`productDescription ${layout}`}>
-      <p className='productDescription__name'>
-        {name}
-      </p>
+      <p className='productDescription__name'>{name}</p>
       <div className='productDescription__pricingGroup'>
         <div className='productDescription__actual'>
-          { onSale && (
-            <span className='productDescription__price--actual'>
-              {actualPrice}{' '}
-            </span>
-            )}
-          <span className={`${classnames({'price--line' : onSale})} productDescription__price--actual`}>
+          {onSale && <span className='productDescription__price--actual'>{actualPrice} </span>}
+          <span
+            className={`${classnames({ 'price--line': onSale })} productDescription__price--actual`}
+          >
             {regularPrice}
           </span>
-          <span className='productDescription__installments'>
-            {installments}
-          </span>
+          <span className='productDescription__installments'>{installments}</span>
         </div>
-        {/* <div className='productDescription__reg'> */}
-          {/* <span className='productDescription__price--reg'>R$ 109,90</span> */}
-          {/* <span className='productDescription__discountPercent'>(desconto de 30%)</span> */}
-        {/* </div> */}
       </div>
     </section>
   );
