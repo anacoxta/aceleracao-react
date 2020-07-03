f89da83e9e7aca8e622372f8cf899c8992d568a0
LiaLobo <lia.lobo@gmail.com>
2020-07-03 00:51:19 -0300

    add code to use redux devtools in browser

--- src/store/index.js
+++ src/store/index.js
@@ -10,5 +10,5 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, cartReducer);
 
-export const Store = createStore(persistedReducer);
+export const Store = createStore(persistedReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 export const persistor = persistStore(Store);
