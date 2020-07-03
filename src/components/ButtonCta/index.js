0f9ebe506e84e4c90b73e05532cacb0f9f913e17
IsabellaSoares <isabella.soares.28@gmail.com>
2020-07-02 22:52:47 -0300

    feat: add disabled button property

--- src/components/ButtonCta/index.js
+++ src/components/ButtonCta/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './style.scss';
 
-const ButtonCta = ({ text, handleClick, layout }) => {
+const ButtonCta = ({ text, handleClick, layout, isDisabled }) => {
   return (
-    <button className={`buttonCta buttonCta--${layout}`} onClick={handleClick}>
+    <button
+      className={`buttonCta buttonCta--${layout}`}
+      onClick={handleClick}
+      disabled={isDisabled}
+    >
       {text}
     </button>
   );
