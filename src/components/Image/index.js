9b886dc59cb04cf6d9ab3079d80e8f7594a7837c
anacoxta <ana.costa.eduardo@gmail.com>
2020-07-03 02:41:22 -0300

    Set up placeholder image

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Placeholder from '../../assets/placeholder-bg.png';
 
 import './style.scss';
 
 const Image = ({ className, src, alt, discountPrice }) => {
+  let picture;
+  if (src && src !== '') {
+    picture = <img src={src} alt={alt} />;
+  } else {
+    picture = <img src={Placeholder} alt={alt} />;
+  }
+
   return (
     <figure>
-      <img src={src} alt={alt}/>
+      {picture}
       <span className={className}>{discountPrice}</span>
     </figure>
   );
